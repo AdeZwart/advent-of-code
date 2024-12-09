1847303172600ddee5e244e61471f699d5425ea1
André de Zwart <andredezwart@gmail.com>
2024-12-09 20:30:29 +0100

    Add Day 8 part 1

---
+++ 2024/dotnet/AdventOfCode2024/AdventOfCode2024.UnitTests/Day8Tests.cs
@@ -0,0 +1,151 @@
+﻿using AzW.AdventOfCode.Year2024;
+
+namespace AdventOfCode2024.UnitTests
+{
+    public class Day8Tests
+    {
+        private static readonly string[] testSample1 = ["..........", "..........", "..........", "....a.....", "..........", ".....a....", "..........", "..........", "..........", ".........."];
+        private readonly Day8 _day8;
+
+        public Day8Tests()
+        {
+            _day8 = new Day8
+            {
+                Input =
+                [
+                    "............",
+                    "........0...",
+                    ".....0......",
+                    ".......0....",
+                    "....0.......",
+                    "......A.....",
+                    "............",
+                    "............",
+                    "........A...",
+                    ".........A..",
+                    "............",
+                    "............"
+                ]
+            };
+        }
+
+        [Fact]
+        public void Should_Get_All_Antenna_Types()
+        {
+            // Arrange
+            //_day8.Input =
+            //[
+            //    "..........",
+            //    "..........",
+            //    "..........",
+            //    "....a.....",
+            //    "..........",
+            //    ".....a....",
+            //    "..........",
+            //    "..........",
+            //    "..........",
+            //    ".........."
+            //];
+
+            // Act
+            var actualAntennaTypes = _day8.GetAntennaTypes();
+
+            // Assert
+            Assert.Equal(2, actualAntennaTypes.Length);
+            Assert.Contains('0', actualAntennaTypes);
+            Assert.Contains('A', actualAntennaTypes);
+        }
+
+        [Fact]
+        public void Should_Get_Antenna_Locations()
+        {
+            // Arrange
+            _day8.Input =
+            [
+                "..........",
+                "..........",
+                "..........",
+                "....a.....",
+                "..........",
+                ".....a....",
+                "..........",
+                "..........",
+                "..........",
+                ".........."
+            ];
+
+            // Act
+            var locations = _day8.GetAntennaLocations('a');
+
+            var actualFirstLocation = locations[0];
+            var actualSecondLocation = locations[1];
+
+            // Assert
+            Assert.Equal(new Coordinate(4, 3), actualFirstLocation);
+            Assert.Equal(new Coordinate(5, 5), actualSecondLocation);
+        }
+
+        [Theory]
+        [InlineData(new[] { 4, 3 }, new[] { 5, 5 }, new[] { 3, 1 }, new[] { 6, 7 })]
+        [InlineData(new[] { 4, 3 }, new[] { 8, 4 }, new[] { 0, 2 }, new[] { 12, 5 })]
+        [InlineData(new[] { 5, 5 }, new[] { 8, 4 }, new[] { 2, 6 }, new[] { 11, 3 })]
+
+        [InlineData(new[] { 8, 1 }, new[] { 5, 2 }, new[] { 11, 0 }, new[] { 2, 3 })]
+        [InlineData(new[] { 8, 1 }, new[] { 7, 3 }, new[] { 9, -1 }, new[] { 6, 5 })]
+        [InlineData(new[] { 8, 1 }, new[] { 4, 4 }, new[] { 12, -2 }, new[] { 0, 7 })]
+
+        [InlineData(new[] { 5, 2 }, new[] { 7, 3 }, new[] { 3, 1 }, new[] { 9, 4 })]
+        [InlineData(new[] { 5, 2 }, new[] { 4, 4 }, new[] { 6, 0 }, new[] { 3, 6 })]
+
+        [InlineData(new[] { 7, 3 }, new[] { 4, 4 }, new[] { 10, 2 }, new[] { 1, 5 })]
+        public void Should_Calculate_Antinodes(int[] firstAntenna, int[] secondAntenna, int[] expectedFirstAntinode, int[] expectedSecondAntinode)
+        {
+            // Arrange
+            var firstAntennaLocation = new Coordinate(firstAntenna[0], firstAntenna[1]);
+            var secondAntennaLocation = new Coordinate(secondAntenna[0], secondAntenna[1]);
+
+            // Act
+            var antinodes = _day8.CalculateAntinodePositions(firstAntennaLocation, secondAntennaLocation);
+            var actualFirstAntinode = antinodes[0];
+            var actualSecondAntinode = antinodes[1];
+
+            // Assert
+            Assert.Equal(new Coordinate(expectedFirstAntinode[0], expectedFirstAntinode[1]), actualFirstAntinode);
+            Assert.Equal(new Coordinate(expectedSecondAntinode[0], expectedSecondAntinode[1]), actualSecondAntinode);
+        }
+
+        [Fact]
+        public void Should_Calculate_Antinodes_With_Resonant_Harmonics()
+        {
+            // Arrange
+            // Act
+
+            // Assert
+
+        }
+
+        [Fact]
+        public void Should_Get_Sum_Of_Unique_Antinodes_When_Executing_Part_1()
+        {
+            // Arrange
+
+            // Act
+            var sumOfUniqueAntinodes = _day8.ExecutePart1();
+
+            // Assert
+            Assert.Equal(14, sumOfUniqueAntinodes);
+        }
+
+        //[Fact]
+        //public void Should_Get_Sum_Of_Unique_Antinodes_When_Executing_Part_2()
+        //{
+        //    // Arrange
+
+        //    // Act
+        //    var sumOfUniqueAntinodes = _day8.ExecutePart2();
+
+        //    // Assert
+        //    Assert.Equal(34, sumOfUniqueAntinodes);
+        //}
+    }
+}
---
+++ 2024/dotnet/AdventOfCode2024/AdventOfCode2024/Day8.cs
@@ -0,0 +1,136 @@
+﻿
+
+
+
+namespace AzW.AdventOfCode.Year2024
+{
+    public class Day8 : Day.NewLineSplitParsed<string>
+    {
+        public Coordinate[] CalculateAntinodePositions(Coordinate firstAntennaLocation, Coordinate secondAntennaLocation)
+        {
+            var locations = new List<Coordinate>();
+
+            var x = firstAntennaLocation.X - secondAntennaLocation.X;
+            x = (x < 0 || firstAntennaLocation.X > secondAntennaLocation.X) ? -x : x;
+
+            var y = firstAntennaLocation.Y - secondAntennaLocation.Y;
+            y = (y < 0 || firstAntennaLocation.Y > secondAntennaLocation.Y) ? -y : y;
+
+            locations.Add(new Coordinate(firstAntennaLocation.X - x, firstAntennaLocation.Y - y));
+            locations.Add(new Coordinate(secondAntennaLocation.X + x, secondAntennaLocation.Y + y));
+
+            return [.. locations];
+        }
+
+        public override object ExecutePart1()
+        {
+            var uniqueAntennaLocations = new HashSet<Coordinate>();
+
+            var antennaTypes = GetAntennaTypes();
+
+            foreach (var antennaType in antennaTypes)
+            {
+                var antennaLocations = GetAntennaLocations(antennaType);
+                foreach (var index in Enumerable.Range(0, antennaLocations.Length - 1))
+                {
+                    foreach (var index2 in Enumerable.Range(index + 1, antennaLocations.Length - (index + 1)))
+                    {
+                        var firstAntenna = antennaLocations[index];
+                        var secondAntenna = antennaLocations[index2];
+                        var antinodes = CalculateAntinodePositions(firstAntenna, secondAntenna);
+
+                        if (IsInGrid(antinodes[0]))
+                        {
+                            uniqueAntennaLocations.Add(antinodes[0]);
+                        }
+
+                        if (IsInGrid(antinodes[1]))
+                        {
+                            uniqueAntennaLocations.Add(antinodes[1]);
+                        }
+                    }
+                }
+            }
+
+            return uniqueAntennaLocations.Count;
+        }
+
+        private bool IsInGrid(Coordinate coordinate)
+        {
+            if (coordinate.X < 0 || coordinate.Y < 0)
+            {
+                return false;
+            }
+
+            if (coordinate.Y >= Input.Length || coordinate.X >= Input[0].Length)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override object ExecutePart2()
+        {
+            var uniqueAntennaLocations = new HashSet<Coordinate>();
+
+            var antennaTypes = GetAntennaTypes();
+
+            foreach (var antennaType in antennaTypes)
+            {
+                var antennaLocations = GetAntennaLocations(antennaType);
+                foreach (var index in Enumerable.Range(0, antennaLocations.Length - 1))
+                {
+                    foreach (var index2 in Enumerable.Range(index + 1, antennaLocations.Length - (index + 1)))
+                    {
+                        var firstAntenna = antennaLocations[index];
+                        var secondAntenna = antennaLocations[index2];
+                        var antinodes = CalculateAntinodePositions(firstAntenna, secondAntenna);
+
+                        if (IsInGrid(antinodes[0]))
+                        {
+                            uniqueAntennaLocations.Add(antinodes[0]);
+                        }
+
+                        if (IsInGrid(antinodes[1]))
+                        {
+                            uniqueAntennaLocations.Add(antinodes[1]);
+                        }
+                    }
+                }
+            }
+
+            return uniqueAntennaLocations.Count;
+        }
+
+        public Coordinate[] GetAntennaLocations(char antenna)
+        {
+            var locations = new List<Coordinate>();
+
+            foreach (var y in Enumerable.Range(0, Input.Length))
+            {
+                foreach (var x in Enumerable.Range(0, Input[0].Length))
+                {
+                    if (Input[y][x] == antenna)
+                    {
+                        locations.Add(new Coordinate(x, y));
+                    }
+                }
+            }
+
+            return [.. locations];
+        }
+
+        public char[] GetAntennaTypes()
+        {
+            // Find all distinct antennas
+            var antennas = string.Join("", Input).Distinct().ToList();
+            // Cleanup the . placeholders
+            antennas.Remove('.');
+
+            return [.. antennas];
+        }
+    }
+
+    public record Coordinate(int X, int Y);
+}
