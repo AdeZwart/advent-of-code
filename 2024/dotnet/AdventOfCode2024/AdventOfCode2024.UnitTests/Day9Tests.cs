66eecc7975f04b21be1f236636de8ff79f0d8bfe
André de Zwart <andredezwart@gmail.com>
2024-12-11 20:23:40 +0100

    Add day 9 file, no solution

---
+++ 2024/dotnet/AdventOfCode2024/AdventOfCode2024.UnitTests/Day9Tests.cs
@@ -0,0 +1,36 @@
+﻿using AzW.AdventOfCode.Year2024;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.UnitTests
+{
+    public class Day9Tests
+    {
+        private readonly Day9 _day9;
+
+        public Day9Tests()
+        {
+            _day9 = new Day9
+            {
+                Input = "2333133121414131402"
+            };
+        }
+
+        [Theory]
+        [InlineData("12345", "0..111....22222")]
+        [InlineData("2333133121414131402", "00...111...2...333.44.5555.6666.777.888899")]
+        public void Decompresses_Disk_Map(string diskMap, string expectedDecompressedDiskMap)
+        {
+            // Arrange
+
+            // Act
+            var decompressedDiskMap = _day9.DecompressDiskMap(diskMap);
+
+            // Assert
+            Assert.Equal(expectedDecompressedDiskMap, decompressedDiskMap);
+        }        
+    }
+}
---
+++ 2024/dotnet/AdventOfCode2024/AdventOfCode2024/Day9.cs
@@ -0,0 +1,36 @@
+﻿namespace AzW.AdventOfCode.Year2024
+{
+    public class Day9 : Day
+    {
+        public string DecompressDiskMap(string diskMap)
+        {
+            var decompressedDiskMap = string.Empty;
+            var currentFileId = 0;
+            foreach (var index in Enumerable.Range(0, diskMap.Length - 1))
+            {
+                if (int.IsEvenInteger(index))
+                {
+                    // Indicates file length
+                    var len = diskMap[index];
+                    
+                }
+                else
+                {
+                    // Indicates free space length
+                }
+            }
+
+            return string.Empty;
+        }
+
+        public override object ExecutePart1()
+        {
+            return base.ExecutePart1();
+        }
+
+        public override object ExecutePart2()
+        {
+            return base.ExecutePart2();
+        }
+    }
+}
